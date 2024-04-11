205b0937abbc1d921ac183f5285b16e8ae987400
hossein <hosseinasadian442@gmail.com>
2024-04-11 12:33:30 +0330

    add swagger to document api list.

--- packages/pishehgostar/product/src/routes/web.php
+++ packages/pishehgostar/product/src/routes/web.php
@@ -1,1 +1,10 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
+
+Route::get('documentation/admin/product',function (){
+    $t = now()->getTimestamp();
+    $url = Storage::disk('documentation')->url("sample.yaml?v=$t");
+    return view('swagger.documentation',compact('url'));
+});
